cb911bbd97ec509c4b50e10af449480ab13145c6
Daniele Vinci <dvinci@danmbp201916.lan>
2020-05-23 23:18:02 -0400

    basic functionality send/receive 'chat' message

---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var socket = require('socket.io');
+
+// App Setup
+var app = express();
+var server = app.listen(4000, () => console.log('listening for request on port 4000'));
+
+// Static files
+app.use(express.static('public'))
+
+// Socket setup: we want socket.io work with server 'server'
+var io = socket(server);
+
+// we call a allback function when a connection is established
+// we have to listen for the message sent from the client (the 'chat' message)
+io.on('connection', (socket) => {
+    console.log('Made socket connection ', socket.id);
+    // we are referring to the socket between the server and the particular client
+    // that is sending the message
+    // when we are receiving the 'chat' message we fire a callback function
+    // we want to send the data to all the other clients connected to the socket 
+    // so that everyone in the chatroom can see the message
+    // 'io.sockets' is referring to all the sockets connected to the server
+    // .emit a message to everyone of those
+    // we have to say what kind of message we're sending back to the clients
+    // we are sending the 'data' we received from the first client
+    socket.on('chat', (data) => {
+        io.sockets.emit('chat', data)
+    });
+})
---
+++ public/chat.js
@@ -0,0 +1,31 @@
+// establishing connection with the server
+
+// socket for frontend (it is different fromthe one in the backend)
+// we have access to it because we have it loaded in the html file with the socket.io CDN
+var socket = io.connect('http://localhost:4000');
+
+// Query DOM
+var message = document.getElementById('message');
+var handle = document.getElementById('handle');
+var btn = document.getElementById('send');
+var output = document.getElementById('output');
+
+// Emit an event(message) and send it on the websocket
+// this will emit a message down the socket to the server,
+// two parameters: 
+// 1 name of the message ('chat' since it is a chat message), 
+// 2 what the message is, the data that we are sending to the server. it is an obj
+// message it is the value of the input field message (the var we declared top)
+btn.addEventListener('click', () => {
+    socket.emit('chat', {
+        message: message.value,
+        handle: handle.value
+    });
+});
+
+// Listen for events 
+// we are listening for the chat message from the server
+// with the data we can outputting them to the dom
+socket.on('chat', data => {
+    output.innerHTML += '<p><strong>' + data.handle + ':</strong>' + data.message + '</p>';
+})
